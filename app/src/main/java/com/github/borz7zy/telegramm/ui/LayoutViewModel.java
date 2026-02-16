9189a15488e65cd80b28a018d7a874da0a674d71
stanislav <borz7zy@proton.me>
2026-02-16 16:55:41 +0100

    remove old file

--- app/src/main/java/com/github/borz7zy/telegramm/ui/LayoutViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.github.borz7zy.telegramm.ui;
-
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class LayoutViewModel extends ViewModel {
-    public final MutableLiveData<Integer> topInset = new MutableLiveData<>(0);
-    public final MutableLiveData<Integer> bottomInset = new MutableLiveData<>(0);
-}
